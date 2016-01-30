5a9f4d32f511d54a0eadd451d8446b08006c3ebe
Dillon Hafer <dh@dillonhafer.com>
2016-01-30 10:06:01 -0700

    Add sidebar for fullscreen template

--- sidebar.php
+++ sidebar.php
@@ -71,6 +71,31 @@
 						</div>
 					</div>
 					
+				<?php } elseif ( is_page_template('template-product-fullscreen.php') ) { ?>
+          <div id="sidebar-container" class="span_4 col">
+            <div class="column-image-header"></div>
+            <div class="column-content">
+              <div class="textwidget">
+                <img class="aligncenter size-full wp-image-888" src="/wp-content/uploads/2012/08/call-today-pipeline-packaging.gif" alt="Call: 1-877-242-1880" onclick="goog_report_conversion('tel:1-877-242-1880')">
+              </div>
+              <div class="divider"></div>
+                  <h3 class="widget-title">Products</h3>
+              <div id="shailan-subpages-5">
+                <ul class="subpages">
+                  <li class="page_item page-item-240"><a href="/products/my-briefcase/" title="My Briefcase" rel="">My Briefcase</a></li>
+                  <li class="page_item page-item-101 current_page_item"><a href="http://pipelinepackaging.com/products/product-overview/" title="Product Overview" rel="">Product Overview</a></li>
+                  <li class="page_item page-item-82"><a href="/products/bottles-cubitainers-and-jars/" title="Bottles, Cubitainers and Jars" rel="">Bottles, Cubitainers and Jars</a></li>
+                  <li class="page_item page-item-84"><a href="/products/cans/" title="Cans" rel="">Cans</a></li>
+                  <li class="page_item page-item-88"><a href="/products/closing-tools/" title="Closing Tools" rel="">Closing Tools</a></li>
+                  <li class="page_item page-item-90"><a href="/products/closures/" title="Closures" rel="">Closures</a></li>
+                  <li class="page_item page-item-92"><a href="/products/drums-and-totes/" title="Drums and Totes" rel="">Drums and Totes</a></li>
+                  <li class="page_item page-item-94"><a href="/products/liners/" title="Liners" rel="">Liners</a></li>
+                  <li class="page_item page-item-97"><a href="/products/pails-and-tubs/" title="Pails and Tubs" rel="">Pails and Tubs</a></li>
+                  <li class="page_item page-item-99"><a href="/products/tubes/" title="Tubes" rel="">Tubes</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
 				<?php } else { ?> 
 				
 					<div id="sidebar-container" class="span_4 col">
